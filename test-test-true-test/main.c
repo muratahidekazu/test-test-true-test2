f0863590c6a8a210a624bc735caae34bd286b6d8
hm <h-murata@ieee.org>
2022-10-02 12:44:54 +0900

    Initial Commit

---
+++ test-test-true-test/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  test-test-true-test
+//
+//  Created by murata on 2022/10/02.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World!\n");
+  return 0;
+}
