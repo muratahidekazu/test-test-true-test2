4f6d1917de24e32a8d5ba563d58f6150bd6be310
H.M <muratahidekazu@mac.com>
2022-10-02 20:58:01 +0900

    bbbb

--- test-test-true-test/main.c
+++ test-test-true-test/main.c
@@ -12,4 +12,5 @@ int main(int argc, const char * argv[]) {
   printf("Hello, World!  murata\n　　commit+push!");
   return 0;
   // 動けうごけ1SSHSSHSSHSSH SSHSSHSSSSSHHHHHHHHHHHHHHHHHHH
+  // 家からだったらあっさりpushできたぞ！
 }
