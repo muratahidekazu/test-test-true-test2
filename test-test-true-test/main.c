71f3c510f1b590f804d54059447a7e3eb816787d
Yuki ohi <b011fe@yamaguchi-u.ac.jp>
2022-10-05 13:57:05 +0900

    aaaaaaiiiiiiiuuuuuueeeeeeooooooo

--- test-test-true-test/main.c
+++ test-test-true-test/main.c
@@ -11,35 +11,4 @@ int main(int argc, const char * argv[]) {
   // insert code here...
   printf("Hello, World!  murata\n　　commit+push!");
   return 0;
-  // 動けうごけ1SSHSSHSSHSSH SSHSSHSSSSSHHHHHHHHHHHHHHHHHHH
-  // 家からだったらあっさりpushできたぞ！これがその証拠だ
-  // 家からpush
-  // 家からpush 10/3 8:24
-  // これは研究室からpush 10/3 8:55]
-  // from lab この行が最後ならね
-  // all-tokenn のはず
-  // はい，これがlabからコマンドラインでのpushです
-  // これも，labからコマンドラインでpushです．
-  // koremo です．git config --unset --global url."https://".insteadOf git://  OK　コマンドラインからcommit できた（Xcodeではだめ）
-  // http,https proxy設定 git --unset で消した
-  // xcodebuild -resolvePackageDependencies -scmProvider system やってみたのがこれ．まずはxcodeからはだめ．で，proxy設定すると．
-  // 再度．再度．再度ですよ．
-  //　commit
-//  hogehoge
-//  jlkajsd
-//  fkaldsjfla
-//  fjlakds
-  // change in my room, and commit and pull from my room
-  // change in my room, and commit and pull from my room
-  // change in my room, and commit and pull from my room
-  // change in my room, and commit and pull from my room
-  // change in my room, and commit and pull from my room
-  // change in my room, and commit and pull from my room
-  // change in my room, and commit and pull from my room
-  // change in my room, and commit and pull from my room
-  // change in my room, and commit and pull from my room
-  // change in my room, and commit and pull from my room
-  // from my room!!!!
-  // from my room in the morning
-  // push from my room 10/4 21:58
 }
