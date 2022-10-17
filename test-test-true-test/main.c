6be76cff88e95b6f14dc9fbc51595836c299a603
hm <h-murata@ieee.org>
2022-10-17 16:59:26 +0900

    担当分を自分のブランチにcommit+push

--- test-test-true-test/main.c
+++ test-test-true-test/main.c
@@ -1,3 +1,20 @@
 // エディタでのコピーペーストではなく，マージ操作で自動的に自分の担当コードを加える練習
 
 
+//////////////////////////////////////////////////////  以下，大井さん
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#define NDATA 100
+
+// 構造体を新たな型として定義して便利に使う（int や double と同じ立場の「型」になる）
+typedef struct {
+  double element[ NDATA ];
+  int nrow;
+  int ncol;
+} Matrix;
+
+// 関数のプロトタイプ宣言：コンパイラに引数の数と形を教えるだけ
+void matrixmultiply( Matrix *, Matrix *, Matrix *);
+void print( Matrix *);
