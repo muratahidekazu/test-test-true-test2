db803fd7834df1c454be484a084b5edd2fa764e7
sugaihayato <b043fe@yamaguchi-u.ac.jp>
2022-10-19 11:13:40 +0900

    sugai

--- test-test-true-test/main.c
+++ test-test-true-test/main.c
@@ -56,7 +56,15 @@
 
 
 //////////////////////// 以下，菅井さん
-
+void print( Matrix *matrix ){
+  for ( int irow = 0 ; irow < matrix->nrow ; irow++ ){
+    for ( int icol = 0 ; icol < matrix->ncol ; icol++ ){
+      printf("%g ",matrix->element[irow * matrix->ncol + icol ]);
+    }
+    printf("\n");
+  }
+  printf("\n");
+}
 
 
 
