03d1cd80771acfa826efcaf4ae9e8ae819878e29
Yuki ohi <b011fe@yamaguchi-u.ac.jp>
2022-10-19 11:59:00 +0900

    変えたよ

--- test-test-true-test/main.c
+++ test-test-true-test/main.c
@@ -1,6 +1,20 @@
 //////////////////////////////////////////////////////  以下，大井さん
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
+#define NDATA 100
 
+// 構造体を新たな型として定義して便利に使う（int や double と同じ立場の「型」になる）
+typedef struct {
+  double element[ NDATA ];
+  int nrow;
+  int ncol;
+} Matrix;
+
+// 関数のプロトタイプ宣言：コンパイラに引数の数と形を教えるだけ
+void matrixmultiply( Matrix *, Matrix *, Matrix *);
+void print( Matrix *);
 
 
 
